86744423a2a78bfca71bb8248ee199ed6dc3260d
David Rajcher <davidrajcher@Davids-MacBook-Pro-2.local>
2022-10-04 16:47:05 +0200

    feat: added footer

---
+++ src/components/layout/footer/footer.jsx
@@ -0,0 +1,9 @@
+import "./footer.css";
+export const Footer = () => {
+  return (
+    <footer>
+      <img src='../assets/avatar.png' />
+      <p>Created by David L. Rajcher</p>
+    </footer>
+  );
+};
